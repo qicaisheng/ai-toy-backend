7551261e212758a06048389fe427a0d17c2f3d90
qicaisheng <qicaisheng@qq.com>
2024-08-09 17:26:14 +0800

    refactor: extract basic package in route package

--- app/api/main.py
+++ app/api/main.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.routes import items, login, users, utils
+from app.api.routes.basic import users, utils, items, login
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
