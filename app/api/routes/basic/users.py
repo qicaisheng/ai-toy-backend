42f4c5910d432cde18b9b2c1d0c80292d61eb9b2
qicaisheng <qicaisheng@qq.com>
2024-08-09 17:48:45 +0800

    refactor: move utils and email to app/basic

--- app/api/routes/basic/users.py
+++ app/api/routes/basic/users.py
@@ -24,7 +24,7 @@ from app.basic.models import (
     UserUpdate,
     UserUpdateMe,
 )
-from app.utils import generate_new_account_email, send_email
+from app.basic.utils import generate_new_account_email, send_email
 
 router = APIRouter()
 
