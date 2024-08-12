00f9958b5063d8ebb115bd303be77078ad34372d
qicaisheng <qicaisheng@qq.com>
2024-08-12 13:36:38 +0800

    refactor: remove conversations.py to route.py

--- app/api/main.py
+++ app/api/main.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from app.api.routes.basic import users, utils, items, login
-from app.api.routes import conversations
+from app.api.routes import route
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
