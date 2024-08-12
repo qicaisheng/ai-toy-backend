b68c7c5754ecfecebaebea060f375f48a537c463
qicaisheng <qicaisheng@qq.com>
2024-08-12 13:49:18 +0800

    fix: fix conversation route

--- app/api/main.py
+++ app/api/main.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 
 from app.api.routes.basic import users, utils, items, login
+from app.conversation import route as conversation
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 api_router.include_router(items.router, prefix="/items", tags=["items"])
-api_router.include_router(conversations.router, prefix="/conversations", tags=["conversations"])
+api_router.include_router(conversation.router, prefix="/conversations", tags=["conversations"])
